0cd6a4af234d582a239b9dd6cfb701fa611eff33
Marco Sciatta <marco.sciatta@gmail.com>
2014-07-29 22:49:06 +0200

    mini server with express 4.0

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // server.js 
 var express 		= require('express');
-var app 				= express();
-var port	 			= process.env.PORT || 8000;
-// var mongoose	= require('mongoose');
-var passport 		= require('passport');
-var flash 			= require('connect-flash');
+var app 			= express();
+var port	 		= process.env.PORT || 8080;
 
+// var mongoose	= require('mongoose');
 var morgan 			= require('morgan');
-var cockieParser 	= require('cookie-parser');
-var bodyParser 	= require('body-parser'); 
+var cookieParser 	= require('cookie-parser');
+var bodyParser 	    = require('body-parser'); 
 var session 		= require('express-session');
+
+app.use(express.static(__dirname+'/public'));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port);
+console.log('JoinUs Server listening on port 8080');
