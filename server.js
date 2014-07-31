4d26bffd87f7cb60f7861073f877f4cfe1f26815
Marco Sciatta <marco.sciatta@gmail.com>
2014-07-31 22:39:54 +0200

    migrate to express 4 new Router

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // server.js 
 var express 		= require('express');
 var app 			= express();
-var port	 		= process.env.PORT || 8080;
 
 var morgan 			= require('morgan');
 var cookieParser 	= require('cookie-parser');
@@ -12,77 +11,107 @@ var mongoose	= require('mongoose');
 //Include models
 var UserEvent = require('./models/events.js');
 
-mongoose.connect('mongodb://127.0.0.1:27017/joinusv1');
-
-
-app.use(express.static(__dirname+'/public'));
 app.use(morgan('dev'));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-//Get all events
-app.get('/api/events',function(req,res){
+mongoose.connect('mongodb://127.0.0.1:27017/joinusv1');
+var port = process.env.PORT || 8080;
 
-	UserEvent.find(function(err,uevents){
-    	if(err) res.send(err);
 
-    	res.json(uevents);
-    });
+var router = express.Router();
+
+router.use(function(req,res,next){
+	console.log('Something is happening');
+	next();
 });
 
-//Save an event
-app.post('/api/events',function(req,res){
+router.get('/',function(req,res){
+	res.json({ message: 'Welcome to JoinUs api'});
+});
 
-	console.log(req.body);
-	 UserEvent.create({
-		name: req.body.name,
-    	description: req.body.description,
-    	date: req.body.date,
-    	author: req.body.author,
-    	isPublic: req.body.isPublic,
-    	type: req.body.type,
-	 }, function(err,uevent){
-	 		if(err)
+
+router.route('/events')
+	.post(function(req,res){
+		console.log(req.body);
+	 	
+	 	UserEvent.create({
+			name: req.body.name,
+	    	description: req.body.description,
+	    	date: req.body.date,
+	    	author: req.body.author,
+	    	isPublic: req.body.isPublic,
+	    	type: req.body.type,
+	 	}, function(err,uevent){
+ 			if(err)
 	 			res.send(err);
 
 	 		UserEvent.find(function(err,uevents){
 	 			if(err) res.send(err);
 	 			res.json(uevents);
  			});
-	 });
-});
+	 	});
+	})
+	.get(function(req,res){
 
-//Get a single event
-app.get('/api/event/:event_id',function(req,res){
-	
-	console.log('Get event with id ' + req.params.event_id);
+		UserEvent.find(function(err,uevents){
+    		if(err) res.send(err);
 
-	UserEvent.findOne({ _id: req.params.event_id },function(err,uevent)
-	{
-		if(err) res.send(err);
-		res.send(uevent);
+    		res.json(uevents);
+    	});
 	});
-});
 
 
 
-//Delete an event
-app.delete('/api/event/:event_id',function(req,res){
+router.route('/event/:event_id')
+	.get(function(req,res){
+	
+		console.log('Get event with id ' + req.params.event_id);
 
-	console.log('Remove event with id '+ req.params.event_id);
+		UserEvent.findOne({ _id: req.params.event_id },function(err,uevent)
+		{
+			if(err) res.send(err);
+			res.send(uevent);
+		});
+	})
+	.put(function(req,res){
+		console.log('Update event with id '+ req.params.event_id);
+		
+		UserEvent.findById(req.params.event_id,function(err,uevent){
 
- 	UserEvent.remove({ _id: req.params.event_id }, function(err,userevent){
- 		if(err) res.send(err);
+			if(err) res.send(err);
+			
+			uevent.name =  req.body.name;
+	    	uevent.description =  req.body.description;
+	    	uevent.date =  req.body.date;
+	    	uevent.author =  req.body.author;
+	    	uevent.isPublic =  req.body.isPublic;
+	    	uevent.type =  req.body.type;
 
- 		UserEvent.find(function(err,events){
- 			if(err) res.send(err);
- 			res.json(events);
+	    	uevent.save(function(err){
+	    		if(err) res.send(err);
+	    		res.json({message: 'success'});
+	    	});
+
+		});
+	})
+	.delete(function(req,res){
+		console.log('Remove event with id '+ req.params.event_id);
+ 		
+ 		UserEvent.remove({ _id: req.params.event_id }, function(err,userevent){
+	 		if(err) res.send(err);
+
+	 		UserEvent.find(function(err,events){
+	 			if(err) res.send(err);
+	 			res.json(events);
+	 		});
  		});
  	});
-	
-});
+
+
+app.use('/api',router);
+
 
 
 
